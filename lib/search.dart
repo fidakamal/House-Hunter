f0ce4a9945a3dc627fcdd866d6c4ff55619beaf7
Fida <n.fida6941@gmail.com>
2022-04-28 17:31:22 +0600

    Refactored Search Class.

--- lib/search.dart
+++ lib/search.dart
@@ -3,54 +3,46 @@ import 'package:geocoding/geocoding.dart';
 import 'package:geoflutterfire/geoflutterfire.dart';
 
 class Search {
-  late String name;
-  late GeoPoint location;
   late double latitude;
   late double longitude;
+  final geo = Geoflutterfire();
+  final _firestore = FirebaseFirestore.instance;
+  static late List<DocumentSnapshot> searchResults;
 
-  void searchRentals(String searchLocation) {
-    getCoordinates(searchLocation);
+  void searchRentals(String searchLocation) async {
+    await getCoordinates(searchLocation);
+    getNearbyRentals();
   }
 
-  void getCoordinates(String searchLocation) async {
+  Future<void> getCoordinates(String searchLocation) async {
     List<Location> coordinates = await locationFromAddress(searchLocation);
     print(coordinates);
     latitude = coordinates[0].latitude;
     longitude = coordinates[0].longitude;
-
-    //getNearbyRentals();
   }
 
   void getNearbyRentals() async {
-    final geo = Geoflutterfire();
-    final _firestore = FirebaseFirestore.instance;
-
-    // GeoFirePoint myLocation =
-    //     geo.point(latitude: 23.7516619, longitude: 90.3747271);
-    // _firestore
-    //     .collection('rentals')
-    //     .add({'name': 'house', 'location': myLocation.data});
-
     GeoFirePoint center = geo.point(latitude: latitude, longitude: longitude);
     var collectionReference = _firestore.collection('rentals');
 
-    var collection = FirebaseFirestore.instance.collection('rentals');
-    var querySnapshot = await collection.get();
-    for (var queryDocumentSnapshot in querySnapshot.docs) {
-      Map<String, dynamic> data = queryDocumentSnapshot.data();
-      var name = data['name'];
-      print(name);
-    }
-
-    double radius = 50;
-    String field = 'location';
+    double radius = 5;
+    String locationField = 'location';
 
     Stream<List<DocumentSnapshot>> stream = geo
         .collection(collectionRef: collectionReference)
-        .within(center: center, radius: radius, field: field);
+        .within(center: center, radius: radius, field: locationField);
 
     stream.listen((List<DocumentSnapshot> documentList) {
+      searchResults = documentList;
       print(documentList[0].data());
     });
+  }
+
+  void addLocation() {
+    GeoFirePoint myLocation =
+        geo.point(latitude: 23.752608, longitude: 90.3762569);
+    _firestore
+        .collection('rentals')
+        .add({'name': 'Hillside Place', 'location': myLocation.data});
   }
 }
