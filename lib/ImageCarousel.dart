505f8b5eae07223c451b73ecab87ed82370d04d2
Alvi Khan <55395950+alvi-khan@users.noreply.github.com>
2022-05-08 01:03:03 +0600

    Added result page image retrieval from firebase.

--- lib/ImageCarousel.dart
+++ lib/ImageCarousel.dart
@@ -2,23 +2,45 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class ImageCarousel extends StatelessWidget {
-  const ImageCarousel({Key? key, required this.onlineImages}) : super(key: key);
-  final List<String> onlineImages;
+  const ImageCarousel({Key? key, required this.images}) : super(key: key);
+  final List<String> images;
 
   @override
   Widget build(BuildContext context) {
     return CarouselSlider(
       options: CarouselOptions(height: 300.0),
-      items: onlineImages.map((image) {
+      items: images.map((image) {
         return Builder(
           builder: (BuildContext context) {
             return Container(
                 margin: EdgeInsets.symmetric(horizontal: 5.0),
-                child: Image.network(image, fit: BoxFit.cover)
+                child: Image.network(
+                    image,
+                    fit: BoxFit.cover,
+                  loadingBuilder: (context, widget, loadingProgress) {
+                      if (loadingProgress == null)  return widget;
+                      return LoadingIndicator();
+                  },
+                )
             );
           },
         );
       }).toList(),
+    );
+  }
+}
+
+class LoadingIndicator extends StatelessWidget {
+  const LoadingIndicator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+        child: SizedBox(
+          width: 100,
+            height: 100,
+            child: CircularProgressIndicator(strokeWidth: 7),
+        )
     );
   }
 }
--- lib/listing.dart
+++ lib/listing.dart
@@ -3,18 +3,45 @@ import 'package:flutter/material.dart';
 import 'package:house_hunter/Navigation.dart';
 import 'package:house_hunter/bottom_navigation.dart';
 import 'package:provider/provider.dart';
-
+import 'package:firebase_storage/firebase_storage.dart';
 import 'ImageCarousel.dart';
 
-class Listing extends StatelessWidget {
-  Listing({Key? key, required this.document}) : super(key: key);
+class Listing extends StatefulWidget {
+  const Listing({Key? key, required this.document}) : super(key: key);
   final DocumentSnapshot? document;
-  var onlineImages = ["https://images.unsplash.com/photo-1570129477492-45c003edd2be?w=1170",
-  "https://images.unsplash.com/photo-1572120360610-d971b9d7767c?w=1170"];
+  @override
+  State<StatefulWidget> createState() => _Listing();
+}
+
+class _Listing extends State<Listing> {
+  List<String> images = [];
+  bool loadingImages = true;
+
+  @override
+  void initState() {
+    super.initState();
+    getImages();
+  }
+
+  void getImages() async {
+    final storage = FirebaseStorage.instance.ref();
+    List<String> imageUrls = [];
+    ListResult images = await storage.child("/rentalImages/${widget.document!.id}").listAll();
+
+    for (var image in images.items) {
+      String url = await image.getDownloadURL();
+      imageUrls.add(url);
+    }
+
+    setState(() {
+      this.images = imageUrls;
+      loadingImages = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    if (document == null) {
+    if (widget.document == null) {
       return Padding(
         padding: const EdgeInsets.all(50.0),
         child: Column(
@@ -44,24 +71,36 @@ class Listing extends StatelessWidget {
       );
     }
 
+    if (loadingImages) {
+      return const Center(
+        child: SizedBox(
+          height: 100,
+          width: 100,
+          child: CircularProgressIndicator(
+            strokeWidth: 10,
+          ),
+        ),
+      );
+    }
+
     return Container(
       padding: EdgeInsets.only(top: 20),
       child: ListView(
           children: [
-            ImageCarousel(onlineImages: onlineImages),
+            ImageCarousel(images: images),
             Container(
               padding: EdgeInsets.all(30),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Text(document!['name'], style: const TextStyle(fontSize: 30)),
+                  Text(widget.document!['name'], style: const TextStyle(fontSize: 30)),
                   const SizedBox(height: 20),
                   Row(
                     children: [
                       const Icon(Icons.phone_rounded, size: 25),
                       const SizedBox(width: 10),
-                      Flexible(child: Text(document!['phone'], style: const TextStyle(fontSize: 20)))
+                      Flexible(child: Text(widget.document!['phone'], style: const TextStyle(fontSize: 20)))
                     ],
                   ),
                   const SizedBox(height: 20),
@@ -69,7 +108,7 @@ class Listing extends StatelessWidget {
                     children: [
                       const Icon(Icons.location_on, size: 25),
                       const SizedBox(width: 10),
-                      Flexible(child: Text(document!['address'], style: const TextStyle(fontSize: 20)))
+                      Flexible(child: Text(widget.document!['address'], style: const TextStyle(fontSize: 20)))
                     ],
                   ),
                   const SizedBox(height: 20),
@@ -77,7 +116,7 @@ class Listing extends StatelessWidget {
                     children: [
                       const Icon(Icons.attach_money_rounded, size: 25),
                       const SizedBox(width: 10),
-                      Text(document!['rent'].toString(), style: const TextStyle(fontSize: 20)),
+                      Text(widget.document!['rent'].toString(), style: const TextStyle(fontSize: 20)),
                     ],
                   ),
                   const SizedBox(height: 20),
@@ -85,11 +124,11 @@ class Listing extends StatelessWidget {
                     children: [
                       const Icon(Icons.bed_rounded, size: 25),
                       const SizedBox(width: 10),
-                      Text(document!['bedrooms'].toString() + " beds", style: const TextStyle(fontSize: 20)),
+                      Text(widget.document!['bedrooms'].toString() + " beds", style: const TextStyle(fontSize: 20)),
                       const SizedBox(width: 20),
                       const Icon(Icons.shower_outlined, size: 25),
                       const SizedBox(width: 10),
-                      Text(document!['baths'].toString() + " baths", style: const TextStyle(fontSize: 20)),
+                      Text(widget.document!['baths'].toString() + " baths", style: const TextStyle(fontSize: 20)),
                     ],
                   ),
                   const SizedBox(height: 20),
@@ -97,7 +136,7 @@ class Listing extends StatelessWidget {
                     children: [
                       const Icon(Icons.architecture_rounded, size: 25),
                       const SizedBox(width: 10),
-                      Flexible(child: Text(document!['size'].toString() + " sq. ft.", style: const TextStyle(fontSize: 20)))
+                      Flexible(child: Text(widget.document!['size'].toString() + " sq. ft.", style: const TextStyle(fontSize: 20)))
                     ],
                   ),
                 ],
