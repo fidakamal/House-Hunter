8cd017c33c9d8d0a476689d24241142657649884
Alvi Khan <55395950+alvi-khan@users.noreply.github.com>
2022-05-17 00:20:07 +0600

    Added error screens on messaging tab.

--- lib/messaging/message_list.dart
+++ lib/messaging/message_list.dart
@@ -1,8 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:house_hunter/Navigation.dart';
+import 'package:house_hunter/bottom_navigation.dart';
 import 'package:house_hunter/message.dart';
-import 'package:house_hunter/profile/pre_logged_in_profile.dart';
+import 'package:provider/provider.dart';
 
 class MessageList extends StatefulWidget {
   MessageList({Key? key}) : super(key: key);
@@ -13,6 +15,15 @@ class MessageList extends StatefulWidget {
 
 class _MessageListState extends State<MessageList> {
   User? user = FirebaseAuth.instance.currentUser;
+  ValueNotifier value = ValueNotifier(FirebaseAuth.instance.currentUser);
+
+  @override
+  void initState() {
+    super.initState();
+    FirebaseAuth.instance.authStateChanges().listen((event) {
+      setState(() => user = FirebaseAuth.instance.currentUser);
+    });
+  }
 
   void goToDM(String receiver) {
     Navigator.push(
@@ -26,9 +37,38 @@ class _MessageListState extends State<MessageList> {
   @override
   Widget build(BuildContext context) {
     if (user == null) {
-      return PreLoggedInProfile(setUser: (newUser) {
-        setState(() => user = FirebaseAuth.instance.currentUser);
-      });
+      return Padding(
+        padding: const EdgeInsets.all(50.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Icon(Icons.person_rounded, size: 80),
+            SizedBox(height: 10.0),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 60),
+              child: Text(
+                  "Please log in to view your messages.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 14, height: 1.5, color: Colors.black38)),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 70),
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.cyanAccent[700],
+                      padding:
+                      EdgeInsets.symmetric(vertical: 17, horizontal: 10),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12))),
+                  onPressed: () => Provider.of<Navigation>(context, listen:false).updateCurrentPage(PageName.profile),
+                  child: Text("Log In")),
+            )
+          ],
+        ),
+      );
     }
 
     return StreamBuilder<QuerySnapshot>(
@@ -39,6 +79,38 @@ class _MessageListState extends State<MessageList> {
             return const Center(
               child: CircularProgressIndicator(
                 backgroundColor: Colors.lightBlueAccent,
+              ),
+            );
+          }
+
+          if (snapshot.data!.docs.isEmpty) {
+            return Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Icon(Icons.message_rounded, size: 80),
+                  SizedBox(height: 10.0),
+                  Text(
+                      "No conversations yet. When a landlord messages you about a property, it will show up here.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 14, height: 1.5, color: Colors.black38)),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 70),
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.cyanAccent[700],
+                            padding:
+                            EdgeInsets.symmetric(vertical: 17, horizontal: 10),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12))),
+                        onPressed: () => Provider.of<Navigation>(context, listen:false).updateCurrentPage(PageName.map),
+                        child: Text("Start Browsing")),
+                  )
+                ],
               ),
             );
           }
